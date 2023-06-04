533c706372cb55bb9ef33e9869b3be24562885c2
christophersharpe <chrismsharpe123@gmail.com>
2023-06-05 00:56:10 +0100

    function now cycles through all image urls to get all 3 images for the card

--- backend/database/getCardImages.js
+++ backend/database/getCardImages.js
@@ -1,26 +1,33 @@
-const axios = require('axios')
-const fs = require('fs')
+const axios = require("axios");
+const fs = require("fs");
 
 const getCardImages = async () => {
-await axios
-  .get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
-  .then((response) => {
-    const data = response.data.data;
-    console.log(data)
-    // Store the data in your own API
+  await axios
+    .get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
+    .then((response) => {
+      const data = response.data.data;
+      // Store the data in your own API
 
-    // Download all the card images
-    data.forEach((card) => {
-      const imageUrl = card.card_images[0].image_url;
-      const imageName = card.id + ".jpg";
-      axios
-        .get(imageUrl, { responseType: "stream" })
-        .then((response) => {
-          response.data.pipe(fs.createWriteStream("/backend/database/card-images" + imageName));
-        })
-        .catch((error) => console.error(error.message));
-    });
-  })
-  .catch((error) => console.error(error));
-}
-getCardImages()
+      // Download all the card images
+      data.forEach((card) => {
+
+      card.card_images.forEach((image, index) => {
+        const imageUrl = image.image_url;
+        const imageName = card.id + "_" + index + ".jpg";
+        axios
+          .get(imageUrl, { responseType: "stream" })
+          .then((response) => {
+            response.data.pipe(
+              fs.createWriteStream("card-images/" + imageName)
+            );
+          })
+          .catch((error) => console.error(error));
+      });
+        const imageUrl = card.card_images[0].image_url;
+        const imageName = card.id + ".jpg";
+      });
+    })
+    .catch((error) => console.error(error));
+};
+fs.mkdirSync("card-images");
+getCardImages();

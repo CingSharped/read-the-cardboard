c1451913451c0c83fba165a94d6c59ea9bf69df6
christophersharpe <chrismsharpe123@gmail.com>
2023-06-04 21:48:31 +0100

    user signup now requires a username property and users can login with username or email and returns custom error for invalid email or username

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -7,33 +7,33 @@ const createToken = (_id) => {
 };
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body
+  const { username, email, password } = req.body
   try {
     // Login user
-    const user = await User.login(email, password)
+    const user = await User.login(username, email, password)
 
     // Create a token for the user
     const token = createToken(user._id)
 
     // Return the email and token
-    res.status(200).json({email, token})
+    res.status(200).json({ username: user.username, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
 }
 
 const signupUser = async (req, res) => {
-  const { email, password } = req.body
+  const { username, email, password } = req.body
 
   try {
     // Signup user
-    const user = await User.signup(email, password)
+    const user = await User.signup(username, email, password)
 
     // Create token
     const token = createToken(user._id)
 
     // Return email and token
-    res.status(201).json({ email, token });
+    res.status(201).json({ username, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
